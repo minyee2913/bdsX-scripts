4b064a2d880d20e6238d92886ef822a3d7f15a9c
minyee2913 <72301973+minyee2913@users.noreply.github.com>
2020-12-28 11:23:51 +0900

    Update Sample.js

--- form/Sample.js
+++ form/Sample.js
@@ -3,25 +3,26 @@ var form = require("./JSform.js");
 
 bdsx.command.hook.on((command, originName) => {
     if (originName != 'Server' && command == '/form') {
-      form.send(target, {
-        type: "form",
-        title: "Sample Form",
-        content: "made by minyee2913",
-        buttons: [
-            {
-                "text": "say 0"
+        var target = form.nIt.get(originName);
+        form.send(target, {
+            type: "form",
+            title: "Sample Form",
+            content: "made by minyee2913",
+            buttons: [
+                {
+                    "text": "say 0"
 
-            },
-            {
-                "text": "say 1"
+                },
+                {
+                    "text": "say 1"
 
-            }
-        ]
-      },data => {
-          var playerName = form.nIt2.get(target);
-          if (data >= 0) system.executeCommand(`say ${data}`, () => {});
-          if (data >= 0) system.executeCommand(`execute "${playerName}" ~ ~ ~ playsound note.hat @s`, () => {});
-      });
+                }
+            ]
+        },data => {
+            var playerName = form.nIt2.get(target);
+            if (data >= 0) system.executeCommand(`say ${data}`, () => {});
+            if (data >= 0) system.executeCommand(`execute "${playerName}" ~ ~ ~ playsound note.hat @s`, () => {});
+        });
     }
 });
 console.log(Sample Form.js is loaded);
