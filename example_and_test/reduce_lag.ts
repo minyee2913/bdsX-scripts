9d58b98a9cf18b1f9e24a7c1b1578fe846507b4b
minyee2913 <72301973+minyee2913@users.noreply.github.com>
2021-04-04 22:43:53 +0900

    Update reduce_lag.ts

--- example_and_test/reduce_lag.ts
+++ example_and_test/reduce_lag.ts
@@ -1,28 +1,13 @@
-import { bedrockServer } from "bdsx";
-
 const system = server.registerSystem(0,0);
 
-let query = system.registerQuery();
-
-let i = setInterval(()=>{
-    let entities = system.getEntitiesFromQuery(query);
-    if (typeof entities === "object") {
-        for (let i = 0; i <= entities.length; i++) {
-            let entity = entities[i];
-            if (typeof entity === "object") {
-                if (entity.__identifier__ !== "minecraft:player") {
-                    if (system.hasComponent(entity, "minecraft:ticking_area_description")) system.destroyComponent(entity, "minecraft:ticking_area_description");
-                    if (system.hasComponent(entity, "minecraft:lookat")) system.destroyComponent(entity, "minecraft:lookat");
-                    return;
-                }
-                return;
+system.listenForEvent("minecraft:entity_created", ev=>{
+    let entity = ev.data.entity;
+    setTimeout(()=>{
+        if (typeof entity === "object") {
+            if (entity.__identifier__ !== "minecraft:player") {
+                if (system.hasComponent(entity, "minecraft:ticking_area_description")) system.destroyComponent(entity, "minecraft:ticking_area_description");
+                if (system.hasComponent(entity, "minecraft:lookat")) system.destroyComponent(entity, "minecraft:lookat");
             }
-            return;
         }
-        return;
-    }
-},5000);
-
-bedrockServer.close.on(()=>{
-    clearInterval(i);
+    },1000);
 });
